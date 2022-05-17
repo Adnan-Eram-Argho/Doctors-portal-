a623c09a557f075f40bf5a1ca84c160e5031e6f0
Your Name <adnaneramargho@gmail.com>
2022-05-18 03:47:42 +0600

    firebase auth done

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -5,8 +5,8 @@ const Service = ({ service, setTreatment }) => {
     //#2A303C
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body">
-                <h2 class="card-title text-secondary">{name}</h2>
+            <div class="card-body text-center">
+                <h2 class="text-xl font-bold text-secondary">{name}</h2>
                 <p>
                     {
                         slots.length ? <span >{slots[0]}</span> : <span className='text-red-500'> Try another day</span>
@@ -16,7 +16,7 @@ const Service = ({ service, setTreatment }) => {
                 <div class="card-actions justify-center">
 
                     <label disabled={slots.length === 0}
-                        onClick={() => setTreatment(service)} for="booking-modal" class="btn btn-secondary text-white uppercase">Book appointment</label>
+                        onClick={() => setTreatment(service)} for="booking-modal" class="btn btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary">Book appointment</label>
                 </div>
 
             </div>
